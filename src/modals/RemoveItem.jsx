c887dbe222480c1864d451f31522f7d9eafa1245
RegistryROK <registry.forbusiness@gmail.com>
2021-06-14 05:45:49 +0900

    fix: apply material-ui to RemoveItem

--- src/modals/RemoveItem.jsx
+++ src/modals/RemoveItem.jsx
@@ -1,15 +1,17 @@
-import style from './stylesheets/RemoveItem.module.css';
-import React from 'react';
+import {makeStyles} from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 import Modal from 'react-modal';
-import Frame from '../components/Frame';
-import MUIButton from '../components/MUIButton';
 
 import {useSelector, useDispatch} from 'react-redux';
 import {modalOpen} from '../reducers/modal';
 import {delMenu} from '../reducers/menuManagement';
 import {resetSelect} from '../reducers/select';
 
-const AddNewItem = () => {
+const RemoveItem = () => {
+  const classes = useStyles();
   const open = useSelector((state) => [...state.modal.open]);
   const select = useSelector((state) => state.select.select);
   const dispatch = useDispatch();
@@ -25,38 +27,78 @@ const AddNewItem = () => {
   };
 
   return (
-    <Modal className={style.root} isOpen={open[1]}>
-      <Frame color={'#F2C94C'} width={624} height={210} left={49} top={54} radius={25}>
-        <p className={style.text}>
-          이상품을 메뉴판에서
-          <br /> 제거 하시겠습니까?
-        </p>
-      </Frame>
-      <MUIButton
-        onClick={close}
-        backColor={'#adff00'}
-        fontSize={36}
-        radius={20}
-        width={624}
-        height={90}
-        left={49}
-        top={289}
-        text={'Back'}
-      />
-      <MUIButton
-        onClick={removeItem}
-        backColor={'#ff006b'}
-        fontColor={'white'}
-        fontSize={48}
-        radius={20}
-        width={624}
-        height={90}
-        left={49}
-        top={394}
-        text={'제거'}
-      />
+    <Modal className={classes.root} isOpen={open[1]}>
+      <Container className={classes.contents} maxWidth={false}>
+        <Typography className={classes.title}>
+          이 상품을 메뉴판에서 <br /> 제거하시겠습니까?
+        </Typography>
+      </Container>
+      <Button className={classes.backB} onClick={close}>
+        Back
+      </Button>
+
+      <Button className={classes.removeB} onClick={removeItem}>
+        제거
+      </Button>
     </Modal>
   );
 };
 
-export default AddNewItem;
+const useStyles = makeStyles({
+  root: {
+    position: 'relative',
+    background: 'linear-gradient(to right, #48c6ef 0%, #6f86d6 100%)',
+    width: 720,
+    height: 520,
+    left: 600,
+    top: 280,
+    borderRadius: 15,
+  },
+  contents: {
+    position: 'absolute',
+    background: '#F2C94C',
+    width: 640,
+    height: 230,
+    left: 40,
+    top: 40,
+    borderRadius: 15,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  title: {
+    position: 'absolute',
+    width: 640,
+    left: 0,
+    fontSize: 36,
+    textAlign: 'center',
+  },
+  backB: {
+    position: 'absolute',
+    background: '#adff00',
+    width: 640,
+    height: 80,
+    left: 40,
+    bottom: 145,
+    borderRadius: 15,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textTransform: 'none',
+    '&:hover': {backgroundColor: '#adff00'},
+  },
+  removeB: {
+    position: 'absolute',
+    background: '#ff006b',
+    width: 640,
+    height: 80,
+    right: 40,
+    bottom: 40,
+    borderRadius: 15,
+    color: 'white',
+    fontSize: 38,
+    fontWeight: 'bold',
+    textTransform: 'none',
+    '&:hover': {backgroundColor: '#ff006b'},
+  },
+});
+
+export default RemoveItem;

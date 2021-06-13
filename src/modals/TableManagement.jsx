272a5e78c3f35e748672baf5ac7515d73fbf57f6
RegistryDev <registry.develop@gmail.com>
2021-06-14 06:32:18 +0900

    fix: apply material-ui to TableManagement

--- src/modals/TableManagement.jsx
+++ src/modals/TableManagement.jsx
@@ -1,20 +1,28 @@
-import style from './stylesheets/TableManagement.module.css';
+import {makeStyles} from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+
 import {useState, useEffect} from 'react';
 import Modal from 'react-modal';
-import Frame from '../components/Frame';
-import MUIButton from '../components/MUIButton';
-import MUITextField from '../components/MUITextField';
 
 import {useSelector, useDispatch} from 'react-redux';
 import {modalOpen} from '../reducers/modal';
 import {postTable, deleteTable} from '../reducers/main';
-import Alert from './Alert';
 
 const TableManagement = () => {
+  const classes = useStyles();
   const open = useSelector((state) => [...state.modal.open]);
   const table = useSelector((state) => [...state.main.table]);
   const [input, setInput] = useState(1);
-  const [text, setText] = useState();
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+  const [message, setMessage] = useState('');
   const regex = /^[0-9]*$/;
 
   const dispatch = useDispatch();
@@ -47,12 +55,12 @@ const TableManagement = () => {
 
   const tableEdit = () => {
     if (input < 1) {
-      setText('테이블은 1개 이상 설치해야 합니다.');
-      dispatch(modalOpen({index: 5, open: true}));
+      setMessage('테이블은 1개 이상 설치해야 합니다.');
+      setOpenSnackBar(true);
       setInput(table.length);
     } else if (input > 100) {
-      setText('테이블 설치 한도 초과입니다.');
-      dispatch(modalOpen({index: 5, open: true}));
+      setMessage('테이블 설치 한도 초과입니다.');
+      setOpenSnackBar(true);
       setInput(table.length);
     } else {
       dispatch(deleteTable());
@@ -67,69 +75,149 @@ const TableManagement = () => {
   };
 
   return (
-    <Modal className={style.TableManagement} isOpen={open[6]}>
-      <Frame color={'#F2C94C'} width={624} height={304} left={49} top={54} radius={25}>
-        <div className={style.name}>테이블 관리</div>
-        <MUITextField
-          backColor={'white'}
-          width={589}
-          left={16}
-          top={128}
-          fontSize={24}
-          radius={'5px'}
+    <Modal className={classes.root} isOpen={open[6]}>
+      <Container className={classes.contents} maxWidth={false}>
+        <Typography className={classes.title}>테이블 관리</Typography>
+
+        <TextField
+          className={classes.count}
           variant={'outlined'}
           value={input}
           onChange={onChange}
         />
-        <MUIButton
-          backColor={'#68DBFF'}
-          fontSize={48}
-          radius={15}
-          width={287}
-          height={59}
-          left={16}
-          top={211}
-          text={'+'}
-          onClick={plus}
-        />
-        <MUIButton
-          backColor={'#68DBFF'}
-          fontSize={48}
-          radius={15}
-          width={287}
-          height={59}
-          left={318}
-          top={211}
-          text={'–'}
-          onClick={minus}
-        />
-      </Frame>
-      <MUIButton
-        onClick={close}
-        backColor={'#adff00'}
-        fontSize={36}
-        radius={20}
-        width={624}
-        height={90}
-        left={49}
-        top={383}
-        text={'Back'}
+
+        <IconButton aria-label="plus" className={classes.plus} onClick={plus}>
+          <AddIcon />
+        </IconButton>
+
+        <IconButton aria-label="minus" className={classes.minus} onClick={minus}>
+          <RemoveIcon />
+        </IconButton>
+      </Container>
+
+      <Button className={classes.backB} onClick={close}>
+        Back
+      </Button>
+
+      <Button className={classes.editB} onClick={tableEdit}>
+        수정
+      </Button>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={openSnackBar}
+        autoHideDuration={1500}
+        onClose={() => setOpenSnackBar(false)}
+        message={message}
+        action={
+          <IconButton
+            aria-label="close"
+            style={{color: 'yellow'}}
+            className={classes.close}
+            onClick={() => setOpenSnackBar(false)}
+          >
+            <CloseIcon />
+          </IconButton>
+        }
       />
-      <MUIButton
-        onClick={tableEdit}
-        backColor={'#ff006b'}
-        fontColor={'white'}
-        fontSize={48}
-        radius={20}
-        width={624}
-        height={90}
-        left={49}
-        top={487}
-        text={'수정'}
-      />
-      <Alert text={text} />
     </Modal>
   );
 };
+
+const useStyles = makeStyles({
+  root: {
+    position: 'relative',
+    background: 'linear-gradient(to right, #48c6ef 0%, #6f86d6 100%)',
+    width: 720,
+    height: 620,
+    left: 600,
+    top: 230,
+    borderRadius: 15,
+  },
+  contents: {
+    position: 'absolute',
+    background: '#F2C94C',
+    width: 640,
+    height: 330,
+    left: 40,
+    top: 40,
+    borderRadius: 15,
+  },
+  title: {
+    position: 'absolute',
+    width: 640,
+    left: 0,
+    top: 40,
+    fontSize: 36,
+    textAlign: 'center',
+  },
+  count: {
+    position: 'absolute',
+    background: 'white',
+    width: 560,
+    left: 40,
+    top: 134,
+    borderRadius: 5,
+    fontSize: 24,
+  },
+  plus: {
+    position: 'absolute',
+    background: '#68DBFF',
+    width: 265,
+    height: 60,
+    left: 40,
+    bottom: 40,
+    borderRadius: 15,
+    color: 'black',
+    fontSize: 36,
+    fontWeight: 'bold',
+    textTransform: 'none',
+    '&:hover': {backgroundColor: '#68DBFF'},
+  },
+  minus: {
+    position: 'absolute',
+    background: '#68DBFF',
+    width: 265,
+    height: 60,
+    right: 40,
+    bottom: 40,
+    borderRadius: 15,
+    color: 'black',
+    fontSize: 36,
+    fontWeight: 'bold',
+    textTransform: 'none',
+    '&:hover': {backgroundColor: '#68DBFF'},
+  },
+  backB: {
+    position: 'absolute',
+    background: '#adff00',
+    width: 640,
+    height: 80,
+    left: 40,
+    bottom: 145,
+    borderRadius: 15,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textTransform: 'none',
+    '&:hover': {backgroundColor: '#adff00'},
+  },
+  editB: {
+    position: 'absolute',
+    background: '#ff006b',
+    width: 640,
+    height: 80,
+    right: 40,
+    bottom: 40,
+    borderRadius: 15,
+    color: 'white',
+    fontSize: 38,
+    fontWeight: 'bold',
+    textTransform: 'none',
+    '&:hover': {backgroundColor: '#ff006b'},
+  },
+});
 
 export default TableManagement;

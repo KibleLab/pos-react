3288fce14a20664af291da392da3caf92cecb6ff
RegistryDev <registry.develop@gmail.com>
2021-06-13 21:08:05 +0900

    fix: MenuButton With Material-UI

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,46 +1,53 @@
-const MenuButton = (props) => {
-  const style = {
-    root: {
-      position: 'absolute',
-      backgroundColor: '#FFC37C',
-      width: 260,
-      height: 155,
-      borderRadius: 15,
-      marginLeft: props.left,
-      marginTop: props.top,
-      cursor: 'default',
-    },
-    name: {
-      position: 'absolute',
-      left: 14,
-      top: 16,
-      fontSize: 24,
-      display: 'flex',
-      alignItems: 'center',
-      textAlign: 'left',
-    },
-    stock: {
-      position: 'absolute',
-      left: 14,
-      top: 110,
-      fontSize: 18,
-    },
-    price: {
-      position: 'absolute',
-      right: 10,
-      top: 110,
-      fontSize: 18,
-      color: '#FF0000',
-    },
-  };
+import {makeStyles} from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
+const MenuButton = (props) => {
+  const classes = useStyles(props);
   return (
-    <div style={style.root} onClick={props.onClick}>
-      <div style={style.name}>{props.name}</div>
-      <div style={style.stock}>재고: {props.stock}</div>
-      <div style={style.price}>{Number(props.price).toLocaleString()}원</div>
-    </div>
+    <Container className={classes.root} maxWidth={false} component={Button} onClick={props.onClick}>
+      <Typography className={classes.name}>{props.name}</Typography>
+      <Typography className={classes.stock}>재고: {props.stock}</Typography>
+      <Typography className={classes.price}>{Number(props.price).toLocaleString()}원</Typography>
+    </Container>
   );
 };
+
+const useStyles = makeStyles({
+  root: (props) => ({
+    position: 'absolute',
+    background: '#FFC37C',
+    width: 260,
+    height: 155,
+    borderRadius: 15,
+    marginLeft: props.left,
+    marginTop: props.top,
+    textTransform: 'none',
+    '&:hover': {backgroundColor: '#FFC37C'},
+  }),
+  name: {
+    position: 'absolute',
+    left: 16,
+    top: 16,
+    fontSize: 24,
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'left',
+  },
+  stock: {
+    position: 'absolute',
+    left: 16,
+    bottom: 16,
+    fontSize: 18,
+  },
+  price: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    fontSize: 18,
+    color: '#FF0000',
+  },
+});
 
 export default MenuButton;

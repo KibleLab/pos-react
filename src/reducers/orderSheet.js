e0f47d0d8597172be91fba8ec3deb40debb471f1
RegistryROK <registry.forbusiness@gmail.com>
2021-07-07 01:47:24 +0900

    fix: logics(with mongoose)

--- src/reducers/orderSheet.js
+++ src/reducers/orderSheet.js
@@ -1,30 +1,38 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getOS = createAsyncThunk('getOS', async (table_no) => {
-  const res = await axios.get(`/api/OrderSheet/${table_no}`);
-  const temp = {table_no: table_no, data: res.data};
+export const getOrder = createAsyncThunk('getOrder', async (table) => {
+  const res = await axios.get(`/ordersheet/${table}`);
+  const temp = {table: table, data: res.data};
   return temp;
 });
 
-export const addOS = createAsyncThunk('addOS', async ({table_no, data}) => {
-  await axios.post(`/api/MenuSelect/${table_no}`, data);
-  const res = await axios.get(`/api/OrderSheet/${table_no}`);
-  const temp = {table_no: table_no, data: res.data};
+export const addOrder = createAsyncThunk('addOrder', async ({table, wishData}) => {
+  const menu_name = wishData.menu_name;
+  const menu_price = wishData.menu_price;
+  const order_quantity = wishData.wish_quantity;
+  await axios.post(`/ordersheet/${table}`, {menu_name, menu_price, order_quantity});
+  const res = await axios.get(`/ordersheet/${table}`);
+  const temp = {table: table, data: res.data};
   return temp;
 });
 
-export const quanIncrOS = createAsyncThunk('quanIncrOS', async ({table_no, data}) => {
-  await axios.patch(`/api/MenuSelect/${table_no}`, data);
-  const res = await axios.get(`/api/OrderSheet/${table_no}`);
-  const temp = {table_no: table_no, data: res.data};
-  return temp;
-});
+export const quanIncrOrder = createAsyncThunk(
+  'quanIncrOS',
+  async ({table, wishData, orderData}) => {
+    const menu_name = orderData.menu_name;
+    const order_quantity = orderData.order_quantity + wishData.wish_quantity;
+    await axios.patch(`/ordersheet/${table}`, {menu_name, order_quantity});
+    const res = await axios.get(`/ordersheet/${table}`);
+    const temp = {table: table, data: res.data};
+    return temp;
+  }
+);
 
-export const resetOS = createAsyncThunk('resetOS', async (table_no) => {
-  await axios.delete(`/api/OrderSheet/${table_no}`);
-  const res = await axios.get(`/api/OrderSheet/${table_no}`);
-  const temp = {table_no: table_no, data: res.data};
+export const resetOrder = createAsyncThunk('resetOrder', async (table) => {
+  await axios.delete(`/ordersheet/${table}`);
+  const res = await axios.get(`/ordersheet/${table}`);
+  const temp = {table: table, data: res.data};
   return temp;
 });
 
@@ -37,19 +45,18 @@ const initialState = {
 const orderSheetSlice = createSlice({
   name: 'orderSheet',
   initialState,
-
   extraReducers: {
-    [getOS.fulfilled]: (state, {payload}) => {
-      state.order[payload.table_no - 1] = [...payload.data];
+    [getOrder.fulfilled]: (state, {payload}) => {
+      state.order[payload.table - 1] = [...payload.data];
     },
-    [addOS.fulfilled]: (state, {payload}) => {
-      state.order[payload.table_no - 1] = [...payload.data];
+    [addOrder.fulfilled]: (state, {payload}) => {
+      state.order[payload.table - 1] = [...payload.data];
     },
-    [quanIncrOS.fulfilled]: (state, {payload}) => {
-      state.order[payload.table_no - 1] = [...payload.data];
+    [quanIncrOrder.fulfilled]: (state, {payload}) => {
+      state.order[payload.table - 1] = [...payload.data];
     },
-    [resetOS.fulfilled]: (state, {payload}) => {
-      state.order[payload.table_no - 1] = [...payload.data];
+    [resetOrder.fulfilled]: (state, {payload}) => {
+      state.order[payload.table - 1] = [...payload.data];
     },
   },
 });

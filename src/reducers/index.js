851f31372410397a2c45b41f6d1a609d49e9e0f6
RegistryROK <registry.forbusiness@gmail.com>
2021-07-07 01:46:18 +0900

    add: reducers (tableMgnt, menuMgnt, menuSlct, wishList)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,22 @@
 import {combineReducers} from 'redux';
-import menuManagement from './menuManagement';
-import menuSelect from './menuSelect';
+import menuMgnt from './menuMgnt';
+import menuSlct from './menuSlct';
+import wishList from './wishList';
 import orderSheet from './orderSheet';
+import dailySales from './dailySales';
+import tableMgnt from './tableMgnt';
 import modal from './modal';
 import select from './select';
-import dailySales from './dailySales';
-import main from './main';
 
 const rootReducer = combineReducers({
-  menuManagement,
-  menuSelect,
+  menuMgnt,
+  menuSlct,
+  wishList,
   orderSheet,
+  dailySales,
+  tableMgnt,
   modal,
   select,
-  dailySales,
-  main,
 });
 
 export default rootReducer;

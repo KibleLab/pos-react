338ef64753873a3fa97d422867779321bef9c086
RegistryROK <registry.forbusiness@gmail.com>
2021-07-21 13:11:09 +0900

    add: proxy menually

---
+++ src/setupProxy.js
@@ -0,0 +1,22 @@
+const createProxyMiddleware = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  // proxy api calls
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:3050/',
+      changeOrigin: true,
+    })
+  );
+
+  // proxy websocket
+  app.use(
+    '/socket',
+    createProxyMiddleware({
+      target: 'http://localhost:3050/',
+      changeOrigin: true,
+      ws: true,
+    })
+  );
+};

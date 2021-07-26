c480033dad6e3a9afbeba85e35b3ee3984d08296
RegistryDev <registry.develop@gmail.com>
2021-07-26 19:42:09 +0900

    fix: edit redux-toolkit to reflect redux-saga

--- src/reducers/orderSheet.js
+++ src/reducers/orderSheet.js
@@ -1,64 +1,99 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import axios from 'axios';
-
-export const getOrder = createAsyncThunk('getOrder', async (table) => {
-  const res = await axios.get(`/api/ordersheet/${table}`);
-  const temp = {table: table, data: res.data};
-  return temp;
-});
-
-export const addOrder = createAsyncThunk('addOrder', async ({table, wishData}) => {
-  const menu_name = wishData.menu_name;
-  const menu_price = wishData.menu_price;
-  const order_quantity = wishData.wish_quantity;
-  await axios.post(`/api/ordersheet/${table}`, {menu_name, menu_price, order_quantity});
-  const res = await axios.get(`/api/ordersheet/${table}`);
-  const temp = {table: table, data: res.data};
-  return temp;
-});
-
-export const quanIncrOrder = createAsyncThunk(
-  'quanIncrOS',
-  async ({table, wishData, orderData}) => {
-    const menu_name = orderData.menu_name;
-    const order_quantity = orderData.order_quantity + wishData.wish_quantity;
-    await axios.patch(`/api/ordersheet/${table}`, {menu_name, order_quantity});
-    const res = await axios.get(`/api/ordersheet/${table}`);
-    const temp = {table: table, data: res.data};
-    return temp;
-  }
-);
-
-export const resetOrder = createAsyncThunk('resetOrder', async (table) => {
-  await axios.delete(`/api/ordersheet/${table}`);
-  const res = await axios.get(`/api/ordersheet/${table}`);
-  const temp = {table: table, data: res.data};
-  return temp;
-});
+import {createSlice} from '@reduxjs/toolkit';
 
 const tableLength = Array.from(Array(100), () => new Array(0));
 
 const initialState = {
   order: [...tableLength],
+  getOrderLoading: false,
+  getOrderDone: false,
+  getOrderError: null,
+  addOrderLoading: false,
+  addOrderDone: false,
+  addOrderError: null,
+  quanIncrLoading: false,
+  quanIncrDone: false,
+  quanIncrError: null,
+  resetOrderLoading: false,
+  resetOrderDone: false,
+  resetOrderError: null,
 };
 
 const orderSheetSlice = createSlice({
   name: 'orderSheet',
   initialState,
-  extraReducers: {
-    [getOrder.fulfilled]: (state, {payload}) => {
-      state.order[payload.table - 1] = [...payload.data];
+  reducers: {
+    GET_ORDER_ORDER_SHEET_REQUEST: (state) => {
+      state.getOrderLoading = true;
+      state.getOrderDone = false;
+      state.getOrderError = null;
     },
-    [addOrder.fulfilled]: (state, {payload}) => {
-      state.order[payload.table - 1] = [...payload.data];
+    GET_ORDER_ORDER_SHEET_SUCCESS: (state, action) => {
+      state.getOrderLoading = false;
+      state.getOrderDone = true;
+      state.order[action.table - 1] = [...action.data];
     },
-    [quanIncrOrder.fulfilled]: (state, {payload}) => {
-      state.order[payload.table - 1] = [...payload.data];
+    GET_ORDER_ORDER_SHEET_FAILURE: (state, action) => {
+      state.getOrderLoading = false;
+      state.getOrderError = action.error;
     },
-    [resetOrder.fulfilled]: (state, {payload}) => {
-      state.order[payload.table - 1] = [...payload.data];
+    ADD_ORDER_ORDER_SHEET_REQUEST: (state) => {
+      state.addOrderLoading = true;
+      state.addOrderDone = false;
+      state.addOrderError = null;
+    },
+    ADD_ORDER_ORDER_SHEET_SUCCESS: (state, action) => {
+      state.addOrderLoading = false;
+      state.addOrderDone = true;
+      state.order[action.table - 1] = [...action.data];
+    },
+    ADD_ORDER_ORDER_SHEET_FAILURE: (state, action) => {
+      state.addOrderLoading = false;
+      state.addOrderError = action.error;
+    },
+    QUAN_INCR_ORDER_SHEET_REQUEST: (state) => {
+      state.quanIncrLoading = true;
+      state.quanIncrDone = false;
+      state.quanIncrError = null;
+    },
+    QUAN_INCR_ORDER_SHEET_SUCCESS: (state, action) => {
+      state.quanIncrLoading = false;
+      state.quanIncrDone = true;
+      state.order[action.table - 1] = [...action.data];
+    },
+    QUAN_INCR_ORDER_SHEET_FAILURE: (state, action) => {
+      state.quanIncrLoading = false;
+      state.quanIncrError = action.error;
+    },
+    RESET_ORDER_ORDER_SHEET_REQUEST: (state) => {
+      state.resetOrderLoading = true;
+      state.resetOrderDone = false;
+      state.resetOrderError = null;
+    },
+    RESET_ORDER_ORDER_SHEET_SUCCESS: (state, action) => {
+      state.resetOrderLoading = false;
+      state.resetOrderDone = true;
+      state.order[action.table - 1] = [...action.data];
+    },
+    RESET_ORDER_ORDER_SHEET_FAILURE: (state, action) => {
+      state.resetOrderLoading = false;
+      state.resetOrderError = action.error;
     },
   },
 });
+
+export const {
+  GET_ORDER_ORDER_SHEET_REQUEST,
+  GET_ORDER_ORDER_SHEET_SUCCESS,
+  GET_ORDER_ORDER_SHEET_FAILURE,
+  ADD_ORDER_ORDER_SHEET_REQUEST,
+  ADD_ORDER_ORDER_SHEET_SUCCESS,
+  ADD_ORDER_ORDER_SHEET_FAILURE,
+  QUAN_INCR_ORDER_SHEET_REQUEST,
+  QUAN_INCR_ORDER_SHEET_SUCCESS,
+  QUAN_INCR_ORDER_SHEET_FAILURE,
+  RESET_ORDER_ORDER_SHEET_REQUEST,
+  RESET_ORDER_ORDER_SHEET_SUCCESS,
+  RESET_ORDER_ORDER_SHEET_FAILURE,
+} = orderSheetSlice.actions;
 
 export default orderSheetSlice.reducer;

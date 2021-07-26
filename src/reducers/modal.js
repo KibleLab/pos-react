8b0dd86a055bf9dca0be720d2a0b5b0ad76cea2c
RegistryROK <registry.forbusiness@gmail.com>
2021-07-26 19:42:34 +0900

    fix: reducer action name

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -8,13 +8,13 @@ const modalSlice = createSlice({
   name: 'modal',
   initialState,
   reducers: {
-    modalOpen: (state, action) => {
+    MODAL_OPEN: (state, action) => {
       state.open[action.payload.index] = action.payload.open;
       state.open = [...state.open];
     },
   },
 });
 
-export const {modalOpen} = modalSlice.actions;
+export const {MODAL_OPEN} = modalSlice.actions;
 
 export default modalSlice.reducer;

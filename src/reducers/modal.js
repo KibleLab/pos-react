e658200264c5b5459c8c64d7abcd7b954256672c
RegistryDev <registry.develop@gmail.com>
2021-08-29 00:29:49 +0900

    fix: apply saga to modal

--- src/reducers/modal.js
+++ src/reducers/modal.js
@@ -2,19 +2,34 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   open: [],
+  isLoading: false,
+  isDone: false,
+  error: null,
 };
 
 const modalSlice = createSlice({
   name: 'modal',
   initialState,
   reducers: {
-    MODAL_OPEN: (state, action) => {
+    MODAL_OPEN_MODAL_REQUEST: (state) => {
+      state.isLoading = true;
+      state.isDone = false;
+      state.error = null;
+    },
+    MODAL_OPEN_MODAL_SUCCESS: (state, action) => {
+      state.isLoading = false;
+      state.isDone = true;
       state.open[action.payload.index] = action.payload.open;
       state.open = [...state.open];
+    },
+    MODAL_OPEN_MODAL_FAILURE: (state, action) => {
+      state.isLoading = true;
+      state.error = action.payload.error;
     },
   },
 });
 
-export const { MODAL_OPEN } = modalSlice.actions;
+export const { MODAL_OPEN_MODAL_REQUEST, MODAL_OPEN_MODAL_SUCCESS, MODAL_OPEN_MODAL_FAILURE } =
+  modalSlice.actions;
 
 export default modalSlice.reducer;

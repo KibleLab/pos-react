09658b6a197be2441cc4b1669ba7c837a8a085f4
RegistryDev <registry.develop@gmail.com>
2021-09-29 01:04:29 +0900

    fix: migration index.jsx to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Main from './containers/Main';
 import MenuMgnt from './containers/MenuMgnt';
-import OrderSheet from './containers/OrderSheet';
 import MenuSlct from './containers/MenuSlct';
+import OrderSheet from './containers/OrderSheet';
 import DailySales from './containers/DailySales';
 import ErrorPage from './containers/ErrorPage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
@@ -18,14 +19,17 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({ reducer: rootReducer, middleware: [sagaMiddleware] });
 sagaMiddleware.run(rootSaga);
 
+export type RootState = ReturnType<typeof store.getState>;
+export type RootDispatch = typeof store.dispatch;
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
         <Route path='/' exact component={Main} />
         <Route path='/menu-mgnt' component={MenuMgnt} />
-        <Route path='/ordersheet/:table' component={OrderSheet} />
         <Route path='/menu-slct/:table' component={MenuSlct} />
+        <Route path='/ordersheet/:table' component={OrderSheet} />
         <Route path='/dailysales' component={DailySales} />
         <Route path='*' component={ErrorPage} />
       </Switch>
